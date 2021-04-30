b6f8aa7f03823ed5bad8661e67d764449a168622
AngelluzZ <57042066+AngelluzZ@users.noreply.github.com>
2021-04-29 23:41:29 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,11 @@
 #include <string>
 #include <tchar.h>
 #include <fstream>
+#include <iostream>
+#include "list.h"
+#include "node.h"
 
-using std::wstring;
-using std::to_wstring;
+using namespace std;
 
 #define OPEN_FILE_BUTTON 1
 
@@ -22,34 +24,18 @@ static TCHAR szWindowClass[] = _T("DesktopAPP");
 // The string that appears in the application's title bar.
 static TCHAR szTitle[] = _T("Windows Desktop Guided Tour Application");
 
-
+//Prototipos
+void contador(Node* Inicio, int* total, int* numeroFunciones, wchar_t* path);
 void AddMenus(HWND hWnd);
 void AddControls(HWND hWnd);
 void OpenFile(HWND hWnd);
 void DisplayFile(wchar_t* path);
 
-LRESULT CALLBACK DialogProcedure(
-	HWND hWnd,
-	UINT msg,
-	WPARAM wParam,
-	LPARAM lParam
+LRESULT CALLBACK DialogProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-);
+LRESULT CALLBACK WndProc(_In_ HWND hWnd, _In_ UINT message, _In_ WPARAM wParam, _In_ LPARAM lParam);
 
-LRESULT CALLBACK WndProc(
-	_In_ HWND hWnd,
-	_In_ UINT message,
-	_In_ WPARAM wParam,
-	_In_ LPARAM lParam
-);
-
-int CALLBACK WinMain(
-	_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPSTR     lpCmdLine,
-	_In_ int       nCmdShow
-)
-{
+int CALLBACK WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR     lpCmdLine, _In_ int       nCmdShow){
 
 	WNDCLASSEX wcex;
 
@@ -66,8 +52,7 @@ int CALLBACK WinMain(
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
 
-	if (!RegisterClassEx(&wcex))
-	{
+	if (!RegisterClassEx(&wcex)){
 		MessageBox(NULL,
 			_T("Call to RegisterClassEx failed!"),
 			_T("Windows Desktop Guided Tour"),
@@ -89,8 +74,7 @@ int CALLBACK WinMain(
 		NULL
 	);
 
-	if (!hWnd)
-	{
+	if (!hWnd){
 		MessageBox(NULL,
 			_T("Call to CreateWindow failed!"),
 			_T("Windows Desktop Guided Tour"),
@@ -105,8 +89,7 @@ int CALLBACK WinMain(
 
 	// Main message loop:
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
+	while (GetMessage(&msg, NULL, 0, 0)){
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
@@ -114,18 +97,15 @@ int CALLBACK WinMain(
 	return (int)msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
 
-	switch (message)
-	{
+	switch (message){
 	case WM_CREATE:
 		AddControls(hWnd);
 		AddMenus(hWnd);
 		break;
 	case WM_COMMAND:
-		switch (wParam)
-		{
+		switch (wParam){
 		case OPEN_FILE_BUTTON:
 			OpenFile(hWnd);
 			break;
@@ -145,8 +125,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-void AddMenus(HWND hWnd)
-{
+void AddMenus(HWND hWnd){
 	// Adding Menus Tutorial | Video #2
 	hMenu = CreateMenu();
 	HMENU hInstrucMenu = CreateMenu();
@@ -158,8 +137,7 @@ void AddMenus(HWND hWnd)
 	SetMenu(hWnd, hMenu);
 }
 
-void AddControls(HWND hWnd)
-{
+void AddControls(HWND hWnd){
 	CreateWindow(_T("Button"),
 		_T("Open"),
 		WS_VISIBLE | WS_CHILD | SS_CENTER,
@@ -168,14 +146,13 @@ void AddControls(HWND hWnd)
 		hWnd, (HMENU)OPEN_FILE_BUTTON, NULL, NULL);
 	hEdit = CreateWindow(_T("Edit"),
 		_T(""),
-		WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE,
+		WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | WS_VSCROLL | WS_HSCROLL,
 		10, 40,
 		280, 130,
 		hWnd, NULL, NULL, NULL);
 }
 
-void OpenFile(HWND hWnd)
-{
+void OpenFile(HWND hWnd){
 	OPENFILENAME ofn;
 	ZeroMemory(&ofn, sizeof(OPENFILENAME));
 
@@ -192,63 +169,167 @@ void OpenFile(HWND hWnd)
 	DisplayFile(ofn.lpstrFile);
 }
 
-void DisplayFile(wchar_t* path)
-{
-	std::fstream file;
-	file.open(path);
+void DisplayFile(wchar_t* path){
+	
+	int total, numeroFunciones=0;
+	Node* Inicio = CrearNode();
+	Node* ptr = Inicio;
 
-	if (!file.is_open())
-	{
-		MessageBox(NULL, _T("Error al cargar archivo"), _T("El archivo no se pudo abrir."), MB_ICONERROR);
-		return;
+	contador(Inicio, &total, &numeroFunciones, path);
+
+	wstring out = _T("Total de LOC: ");
+	wstring data = to_wstring(total);
+	out += data;
+	
+	for (int i = 0; i < numeroFunciones; i++) {
+		ptr = ptr->next;
+		out += _T("\r\nFuncion: ");
+		out += (ptr->data.nombre);
+		out += _T("  Total de LOC: ");
+		data = to_wstring(ptr->data.numeroLineas);
+		out += data;
 	}
 
-	int counter = 0;
-	char line[256];
-	int i = 0;
-	unsigned int lineType = 0b0;
-
-	file.getline(line, 256);
-
-	do
+	ptr = Inicio;
+	while (ptr->next != NULL)
 	{
-		i = 0;
-		lineType = 0;
-		while (line[i] != '\0') {
-			if (line[i] == '/') {
-				if (line[i] == line[i + 1]) {
-					lineType = 0b0010;
-					break;
-				}
-				else if (line[i] == '*'){
-					do {
-						file.getline(line, 256);
-					} while (line[i] == '*' && line[i + 1] == '*');
-					break;
-				}
-			}
-			else if ((line[i] == '}') || (line[i] == ')')){
-				lineType = 0b0100;
-				break;
-			}
-			else if (line[i] != ' ') {
-				lineType = 0b0001;
-				break;
-			}
-			i++;
-		}
-		if (lineType == 0b0001) {
-			counter++;
-		}
-		file.getline(line, 256);
-	} while (!file.eof());
-
-	wstring out = _T("Tu código tiene ");
-	wstring data = to_wstring(counter);
-	out += data;
-	out += _T(" líneas físicas de código.");
+		TIPO datos;
+		if (eliminaNodoLista(ptr, &datos))
+		exit(EXIT_FAILURE);
+	}
+	EliminaNode(Inicio);
 
 	SetWindowText(hEdit, &out[0] );
 
-	file.close();
+
+}
+
+void contador(Node* Inicio, int * total, int* numeroFunciones, wchar_t* path) {
+	
+	//abrir archivo
+	fstream archivo;
+	archivo.open(path, fstream::in);
+	
+
+	string linea;
+	int i, lineasFuncion, llaves, tamanoNombre;
+	int lineasTotales = 0,  comentario;
+	string aux;
+	char* nombreFuncion;
+	unsigned int tipoLinea = 0b0;
+	Node* ptr = Inicio;
+	TIPO datosFuncion;
+	//leer primera linea
+	getline(archivo, linea);
+
+	while (!archivo.eof()) {
+		comentario = linea.find("//");
+		//Es funcion ?
+		if (linea.find("(") !=  string::npos && linea.find(")") != string::npos && linea.find("{") != string::npos && linea.find("while") == string::npos && linea.find("if") == string::npos && linea.find("for(") == string::npos) {
+			*numeroFunciones = *numeroFunciones + 1;
+
+			aux = linea;
+			tamanoNombre = linea.find("(");
+			i = tamanoNombre;
+			aux.erase(i, linea.size());
+			i--;
+			while (linea[i] != ' '){
+				i--;
+			}
+			aux.erase(0,i);
+			nombreFuncion = &aux[0];
+
+			size_t newsize = strlen(nombreFuncion) + 1;
+			wchar_t* wcstring = new wchar_t[newsize];
+
+			// Convert char* string to a wchar_t* string.
+			size_t convertedChars = 0;
+			mbstowcs_s(&convertedChars, wcstring, newsize, nombreFuncion, _TRUNCATE);
+			// Display the result and indicate the type of string that it is.
+			wcout << wcstring << _T(" (wchar_t *)") << endl;
+			datosFuncion.nombre = wcstring;
+
+
+			llaves = 1; // hay una llave
+			lineasFuncion = 1;
+			while (llaves != 0) {
+
+				getline(archivo, linea);
+				i = 0;
+
+				if (comentario != string::npos) { // tiene comentario
+					if (linea.find("{") != string::npos && linea.find("{") < comentario ) {
+						lineasFuncion++;
+						lineasTotales++;
+						llaves++;
+					}
+					if (linea.find(";") != string::npos && linea.find(";") < comentario) {
+						lineasFuncion++;
+						lineasTotales++;
+					}
+					else if (linea.find("}") != string::npos && linea.find("}") < comentario) {
+						llaves--;
+					}
+				}
+				else {
+					if (linea.find("{") != string::npos) {
+						lineasFuncion++;
+						lineasTotales++;
+						llaves++;
+					}
+					else if (linea.find(";") != string::npos ) {
+						lineasFuncion++;
+						lineasTotales++;
+					}
+					if (linea.find("}") != string::npos) {
+						llaves--;
+					}
+				}
+			}
+
+			datosFuncion.numeroLineas = lineasFuncion;
+			if (!insertNodoLista(ptr, datosFuncion)) {
+				ptr = ptr->next;
+			}
+			else {
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if(linea.find("/*") != string::npos){ //Es banner ?
+			do {
+				getline(archivo, linea);
+					lineasTotales++;
+			} while (linea.find("*/") == string::npos);
+		}
+		else if (comentario != string::npos) { // tiene comentario ?
+			if (linea.find("{") != string::npos && linea.find("{") < comentario) {
+				lineasTotales++;
+			}
+			else if (linea.find("#") != string::npos && linea.find("#") < comentario) {
+				lineasTotales++;
+			}
+			else if (linea.find(";") != string::npos && linea.find(";") < comentario) {
+				lineasTotales++;
+			}
+		}
+		else {
+			if (linea.find("{") != string::npos) {
+				lineasTotales++;
+			}
+			else if (linea.find("#") != string::npos) {
+				lineasTotales++;
+			}
+			else if (linea.find(";") != string::npos) {
+				lineasTotales++;
+			}
+		}
+
+		getline(archivo, linea);
+	}
+
+	*total = lineasTotales;
+
+	archivo.close();
+
+	return;
 }
